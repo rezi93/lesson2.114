23c725fd3d517b8e1964f7da8814b15a56f494ca
rezi93 <103065023+rezi93@users.noreply.github.com>
2022-05-15 21:26:46 +0400

    add js

---
+++ script.js
@@ -0,0 +1,45 @@
+// function testfunction(...x) {
+//     console.log(x);
+//     let numbers=0
+//     for ( let y of x){
+//         numbers=numbers+y;
+//         // console.log(numbers);
+//     }
+//     return numbers;
+// }
+// let sum = testfunction(10, 50, 6, 7, 8, 11, 6, 3, 9);
+// console.log(sum);
+
+
+// function testfunction(...y) {
+//     numbers=0
+//     for (let x of y){
+//         numbers=numbers+x
+//         if (x > 0){
+            
+//             console.log(x);
+            
+            
+//         }
+//         // numbers = 0
+//         // for (let b of x){
+//         // numbers = numbers+b;
+//         // console.log(numbers);
+//         // }
+//     }
+// }  
+    
+
+// testfunction (2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+
+
+let user = {
+    firstname: 'giorgi',
+    lastname: 'saakadze',
+    age: 32,
+    isloggedin: true
+  }
+
+if (user.isloggedin=='true') {
+    console.log(giorgi );
+} 
